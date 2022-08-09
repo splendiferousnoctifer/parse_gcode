974b55c15076af834e3788b4f04890a8af052421
splendiferousnoctifer <s2110629012@students.fh-hagenberg.at>
2022-08-09 09:23:05 +0200

    file creation

--- natural_touch.py
+++ natural_touch.py
@@ -3,9 +3,9 @@ from gcody import gcode, gsettings
 import numpy as np
 np.set_printoptions(suppress=True) # don't use scientific notation
 
-AMOUNT_COORDS = 50
+AMOUNT_COORDS = 1000
 GCODE_LINES = AMOUNT_COORDS*2
-X_LIM, Y_LIM, Z_LIM = 20, 110, 80
+X_LIM, Y_LIM, Z_LIM = 20, 110, 85
 TOUCH = 20
 
 def get_coordinates():
@@ -25,7 +25,8 @@ def get_coordinates():
     gc[i + 1] = c[int(i/2)]
 
     if i > 0:
-      interim = np.add(gc[i-1], gc[i+1])/2
+      #interim = np.add(gc[i-1], gc[i+1])/2
+      interim = gc[i+1]
       gc[i] = (0, interim[1], interim[2])
 
   gc[GCODE_LINES] = (0, Y_LIM, Z_LIM)
@@ -37,16 +38,31 @@ c = get_coordinates()
 gset = gsettings(graphics='myplotlib')
 g = gcode(settings = gset)
 g.move(0,0,0, speed = 100)
+g.move(X_LIM,0,0)
+g.dwell(milisec=5000)
+
+g.move(X_LIM, Y_LIM, 0)
+g.move(X_LIM, Y_LIM, Z_LIM)x
+g.move(X_LIM, 0, Z_LIM)
+g.move(X_LIM,0,0)
+
+
 g.dwell(milisec=1000)
-g.move(X_LIM, Y_LIM/2, Z_LIM/2)
-g.dwell(milisec=1000)
+g.move(0, 0, 0)
+
+
 
 for i in range(0,GCODE_LINES+1):
-    g.move(c[i,0], c[i,1], c[i,2])
-    if i % 2:
-        g.dwell(milisec=500)
+  if i == 1:
+    g.move(0, c[i,1], c[i,2])
+  g.move(c[i,0], c[i,1], c[i,2])
+  if i % 2:
+      g.dwell(milisec=500)
+      g.move(0, c[i,1], c[i,2])
 
-g.save('try.gcode')
+
+g.save('ten.gcode')
+g.slide_view()
 
 
 
